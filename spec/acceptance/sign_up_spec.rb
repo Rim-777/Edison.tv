5dbf5341ce7859a0d4da34b542b938101f7c4c02
Rim-777 <javatimomoss@gmail.com>
2016-04-22 03:29:54 +0300

    start registration

---
+++ spec/acceptance/sign_up_spec.rb
@@ -0,0 +1,26 @@
+require_relative 'acceptance_helper'
+
+
+feature 'Sign_up', %q{ create my profile I wont to be able to sign up} do
+
+  scenario 'Un-Registered user is trying to sign up' do
+    visit new_user_registration_path
+
+    fill_in 'user_email', with: 'new_user@test.com'
+    fill_in 'sign_up_password_field', with: '12345678'
+    fill_in 'sign_up_password_confitmation_field', with: '12345678'
+
+    click_on 'Sign up'
+    expect(page).to have_content 'A message with a confirmation link has been sent to your email address. Please follow the link to activate your account.'
+
+  end
+
+  # given(:user) { create(:user) }
+  # scenario 'Registered user is trying to sign up' do
+  #   sign_in(user)
+  #
+  #   visit new_user_registration_path
+  #   expect(page).to have_content 'You are already signed in.'
+  # end
+
+end
