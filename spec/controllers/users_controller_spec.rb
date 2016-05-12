6a53ce8ff8c8b517fa69d173512e896c25132132
Rim-777 <javatimomoss@gmail.com>
2016-05-12 18:50:03 +0300

    avatar testing

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -23,7 +23,7 @@ RSpec.describe UsersController, type: :controller do
     before { sign_in(user) }
 
     context 'User is trying update him-self' do
-      let!(:request){patch :update, {user: attributes_for(:user), avatar: 'qwe.gpg', id: user,  format: :js}}
+      let!(:request){patch :update, {user: attributes_for(:user), avatar: new_avatar, id: user,  format: :js}}
 
       it 'assigns the requested user to @user' do
         patch :update, id: user, user: attributes_for(:user), format: :js
@@ -33,38 +33,11 @@ RSpec.describe UsersController, type: :controller do
       it 'changes user attributes' do
         request
         user.reload
-        expect(user.avatar.file).to eq new_avatar
+        expect(user.avatar_url).to eq new_avatar
       end
-    #
-    #   it 'render temlate update wiev' do
-    #     patch :update, id: question, question: attributes_for(:question), format: :js
-    #     expect(response).to render_template :update
-    #   end
-    # end
-    #
-    #
-    # context 'User is trying update his question with invalid attributes' do
-    #
-    #   it 'does not change question attributes' do
-    #     patch :update, id: question, question: {title: 'new title', body: nil}, format: :js
-    #     question.reload
-    #     expect(question.title).to eq 'OldTitleText'
-    #     expect(question.body).to eq 'OldBodyText'
-    #   end
-    # end
-    #
-    #
-    # context 'User is trying update his not question' do
-    #
-    #   it 'does not change question attributes' do
-    #     sign_in(another_user)
-    #     patch :update, id: question, question: {title: 'new title', body: 'new body'}, format: :js
-    #     question.reload
-    #     expect(question.title).to eq 'OldTitleText'
-    #     expect(question.body).to eq 'OldBodyText'
-    #   end
+
     end
-    #
+
 
   end
 
