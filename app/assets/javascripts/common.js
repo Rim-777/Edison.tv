4307cbc6769965e6e6aaddbb3d8a3301aa0d2256
Rim-777 <javatimomoss@gmail.com>
2016-05-16 04:42:26 +0300

    ref css

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -48,7 +48,7 @@ function setFileUploader() {
                 progress = parseInt(data.loaded / data.total * 100, 10);
                 console.log(progress);
                 data.context.find('.bar').css('width', progress + '%');
-                //$('.progress_modal_body').html(data.context);
+                $('.progress_modal_body').html(data.context);
             }
 
         }
